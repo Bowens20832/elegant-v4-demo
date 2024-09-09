a1d0f9caf711d963f674517f4fd072abccf07ba4
Brandon Owens <brandono@vitamins2you.com>
2024-09-09 18:16:26 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/admin/:path*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
